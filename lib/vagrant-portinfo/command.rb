0e5939e25a47009f68eed81312f2b7310338d858
Jakub Jursa <jju@rsd.com>
2015-04-17 11:48:50 +0200

    fix

--- lib/vagrant-portinfo/command.rb
+++ lib/vagrant-portinfo/command.rb
@@ -17,45 +17,22 @@ module Vagrant
         argv = parse_options(opts)
         return if !argv
 
-        with_target_vms(argv, reverse: true) { |m| print_ports(m) }
+        with_target_vms(argv, reverse: true) { |m| print_portinfo(m) }
       end
 
       private
-      def print_ports(machine)
-        @env.ui.info("#{machine.name} (#{machine.index_uuid[0..6]})")
+      def print_portinfo(machine)
+        @env.ui.info("#{machine.name} (#{machine.index_uuid[0..6]}) #{machine.state.short_description}")
         @env.ui.info("-" * 80)
 
         unless machine.provider_name == :virtualbox
-          print_unknown
+          @env.ui.info("<not implemented for provider #{machine.provider_name}>")
           return
         end
 
-        out = Vagrant::Util::Subprocess.execute("vboxmanage", "showvminfo", machine.id)
-
-        unless out.exit_code.zero?
-          print_unknown
-          return
+        machine.provider.driver.read_forwarded_ports(machine.id, true).each do |i|
+          @env.ui.info("guest: #{i[3]}\thost: #{i[2]}")
         end
-
-        parse_vminfo(out.stdout).each do |i|
-          @env.ui.info("guest: #{i[:guest_port]}\thost: #{i[:host_port]}")
-        end
-      end
-
-      def print_unknown
-        @env.ui.info("<UNKNOWN>")
-      end
-
-      def parse_vminfo(s)
-        out = []
-        s.lines.select { |l| l =~ /guest port/ }.each do |l|
-          out << {
-              :host_port => l[/host\ port\ =\ \d*/, 0][/\d+/].to_i,
-              :guest_port => l[/guest\ port\ =\ \d*/, 0][/\d+/].to_i
-          }
-        end
-
-        out
       end
     end
   end
